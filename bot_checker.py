196a0b05f3eecf757771e0ad01bf7119334aed91
EgorLee <128610903+EgorLee@users.noreply.github.com>
2023-03-31 00:30:02 +0300

    Update bot_checker.py

--- bot_checker.py
+++ bot_checker.py
@@ -73,7 +73,7 @@ def run():
 
 
 if __name__ == '__main__':
-    schedule.every().day.at("00:10").do(run)
+    schedule.every().day.at("00:31").do(run)
     #schedule.every(10).seconds.do(run)
     while True:
         schedule.run_pending()
