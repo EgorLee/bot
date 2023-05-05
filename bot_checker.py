04960f52a6eb1e9a4131a8073aec3b4539da3a58
EgorLee <yahorvasenin@gmail.com>
2023-05-05 09:15:46 +0300

    email

--- bot_checker.py
+++ bot_checker.py
@@ -33,7 +33,7 @@ DATA = {
     #'Content-Type': 'application/json',
     'priceCalculate-contactRadio': 'Phone',
     'name': 'BOT TEST',
-    'tel': '+7 (101) 010-10-10',
+    "email": 'vasenin@lbr.ru',
     'region-delivery': '94',
     "url": "https://www.lbr.ru/traktory/mtz/82-1",
     "model_name": "82.1",
